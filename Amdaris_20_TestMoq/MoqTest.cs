b49b5b75946820df24165b67f0eca02dab1a2ee7
Gabriel Stanescu <darkside_1ro@yahoo.com>
2022-03-26 08:31:47 +0200

    final commit

---
+++ Amdaris_20_TestMoq/MoqTest.cs
@@ -0,0 +1,64 @@
+﻿using Amdaris_20_TDD;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amdaris_20_TestMoq
+{
+    [TestFixture]
+    public class MoqTest
+    {
+        private readonly BankAccount systemUnderTest;
+        private readonly Mock<IOwner> owner = new Mock<IOwner>();
+        private readonly Mock<ILogger> logger = new Mock<ILogger>();
+        private decimal initialAmount = 1000;
+
+        public MoqTest()
+        {
+            systemUnderTest = new BankAccount(owner.Object, logger.Object, initialAmount);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            owner.Name = "Account Owner Mock";
+            owner.Object.FirstName = "FN Mock";
+            owner.Object.LastName = "LN Mock";
+        }
+        [TestCase(500)]
+        [TestCase(0)]
+        [TestCase(-500)]
+        public void Deposit_Should_Update_Balance(decimal amount)
+        {
+            //Arrange
+            decimal initialBalance = systemUnderTest.AccountBalance;
+            //Act
+            systemUnderTest.Deposit(amount);
+            //Assert
+            if (amount > 0)
+                Assert.AreEqual(initialBalance + amount, systemUnderTest.AccountBalance);
+            else
+                Assert.AreEqual(initialBalance, systemUnderTest.AccountBalance);
+        }
+
+        [TestCase(500)]
+        [TestCase(0)]
+        [TestCase(-500)]
+        public void Withdraw_Should_Update_Ballance(decimal amount)
+        {
+            //Arrange
+            decimal initialBalance = systemUnderTest.AccountBalance;
+            //Act
+            systemUnderTest.Withdraw(amount);
+            //Assert
+            if (amount > 0)
+                Assert.AreEqual(initialBalance - amount, systemUnderTest.AccountBalance);
+            else
+                Assert.AreEqual(initialBalance, systemUnderTest.AccountBalance);
+        }
+    }
+}
---
+++ Amdaris_20_TestMoq/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Amdaris_20_TestMoq")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Amdaris_20_TestMoq")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f74dbfdd-2dd4-4fb9-835b-d1fd7d10ad74")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
